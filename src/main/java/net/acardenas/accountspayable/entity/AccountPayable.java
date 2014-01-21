596d7ef795ceeaef4da56248e381e0301a17e04f
Alejandro Cardenas <cardenas.alejandro@gmail.com>
2014-01-21 00:49:57 -0600

    improve entities

--- src/main/java/net/acardenas/accountspayable/entity/AccountPayable.java
+++ src/main/java/net/acardenas/accountspayable/entity/AccountPayable.java
@@ -1,7 +1,10 @@
 package net.acardenas.accountspayable.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 /**
  * Created by acardenas on 1/20/14.
@@ -11,5 +14,13 @@ public class AccountPayable
 {
     @Id
     private Integer id;
+    private String name;
+    private Double total;
+    @OneToMany(mappedBy = "accountPayable", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Payment> payments;
 
+    public AccountPayable()
+    {
+
+    }
 }
---
+++ src/main/java/net/acardenas/accountspayable/entity/Payment.java
@@ -0,0 +1,70 @@
+package net.acardenas.accountspayable.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by acardenas on 1/21/14.
+ */
+@Entity
+public class Payment
+{
+    @Id
+    private Integer id;
+    private String name;
+    private String description;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date effectiveDate;
+    @ManyToOne()
+    private AccountPayable accountPayable;
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public Date getEffectiveDate()
+    {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(Date effectiveDate)
+    {
+        this.effectiveDate = effectiveDate;
+    }
+
+    public AccountPayable getAccountPayable()
+    {
+        return accountPayable;
+    }
+
+    public void setAccountPayable(AccountPayable accountPayable)
+    {
+        this.accountPayable = accountPayable;
+    }
+}
