b2e1f161fd8321b9d8dfceeda43fdf0047ed9e22
Alejandro Cardenas <cardenas.alejandro@gmail.com>
2014-01-20 22:21:08 -0600

    Add JPA support

---
+++ src/main/java/net/acardenas/accountspayable/entity/AccountPayable.java
@@ -0,0 +1,15 @@
+package net.acardenas.accountspayable.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by acardenas on 1/20/14.
+ */
+@Entity
+public class AccountPayable
+{
+    @Id
+    private Integer id;
+
+}
