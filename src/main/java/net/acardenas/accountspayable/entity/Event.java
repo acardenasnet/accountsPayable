1628a5a0969ccdfc171763ac690f37daf1048350
Alejandro Cardenas <cardenas.alejandro@gmail.com>
2014-01-27 00:49:30 -0600

    Added event entity

--- src/main/java/net/acardenas/accountspayable/entity/Event.java
+++ src/main/java/net/acardenas/accountspayable/entity/Event.java
@@ -1,7 +1,38 @@
 package net.acardenas.accountspayable.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
 /**
  * Created by acardenas on 1/25/14.
  */
-public class Event {
+@Entity
+public class Event
+{
+    @Id
+    private Integer id;
+    @OneToMany
+    private List<AccountPayable> accountPayables;
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    public List<AccountPayable> getAccountPayables()
+    {
+        return accountPayables;
+    }
+
+    public void setAccountPayables(List<AccountPayable> accountPayables)
+    {
+        this.accountPayables = accountPayables;
+    }
 }
