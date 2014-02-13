7ee902a14d5112d80e9ab85a017fd867ef94d83b
acardenasnet <cardenas.alejandro@gmail.com>
2014-02-13 12:37:09 -0600

    Fixed Checkstyles

--- src/main/java/net/acardenas/accountspayable/utils/Log.java
+++ src/main/java/net/acardenas/accountspayable/utils/Log.java
@@ -16,7 +16,6 @@ package net.acardenas.accountspayable.utils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.util.ReflectionUtils;
 
@@ -40,27 +39,24 @@ public @interface Log
 class LoggerPostProcessor implements BeanPostProcessor
 {
 
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws
-            BeansException
+    public Object postProcessAfterInitialization(Object bean, String beanName)
     {
         return bean;
     }
 
     public Object postProcessBeforeInitialization(final Object bean, String beanName)
-            throws BeansException
     {
         ReflectionUtils.doWithFields(bean.getClass(), new ReflectionUtils.FieldCallback()
         {
             @SuppressWarnings("unchecked")
-            public void doWith(Field field) throws IllegalArgumentException,
-                    IllegalAccessException
+            public void doWith(Field field) throws IllegalAccessException
             {
                 ReflectionUtils.makeAccessible(field);
 
                 //Check if the field is annoted with @Log
                 if (field.getAnnotation(Log.class) != null)
                 {
-                    Log logAnnotation = field.getAnnotation(Log.class);
+                    field.getAnnotation(Log.class);
                     Logger logger = LoggerFactory.getLogger(bean.getClass());
                     field.set(bean, logger);
                 }
